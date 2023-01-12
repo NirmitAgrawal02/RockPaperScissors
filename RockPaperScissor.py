3c25d176a66b25f206dfdb06543042f4f0824720
Nirmit Agrawal <nagraw18@asu.edu>
2023-01-11 18:55:51 -0700

    Updated GUI

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,8 +1,16 @@
 import random
 import time
-import tkinter
+import tkinter as tk
 
-tkinter._test()
+window = tk.Tk()
+window.geometry("400x300")
+window.title("Rock Paper Scissors Game")
+
+USER_SCORE = 0
+COMP_SCORE = 0
+USER_CHOICE = ""
+COMP_CHOICE = ""
+
 while True:
     n = input("Do you want to play Rock Paper Scissors\n1. Yes\n0.Exit\n")
     if n == 0:
@@ -16,6 +24,8 @@ while True:
         print('-'*30)
         print(f"Both players selected {user_input}. It's a tie!")
         print('-'*30)
+        USER_SCORE += 0.5
+        COMP_SCORE += 0.5
     else:
         if user_input == "Rock":
             if bot == "Scissor":
@@ -23,31 +33,37 @@ while True:
                 print('-'*30)
                 print("User Won")
                 print('-'*30)
+                USER_SCORE += 1
             else:
                 print("Paper Beat Rock")
                 print('-'*30)
                 print("User Lost")
                 print('-'*30)
+                COMP_SCORE += 1
         elif (user_input == "Paper"):
             if bot == "Rock":
                 print("Paper Beat Rock")
                 print('-'*30)
                 print("User Lost")
                 print('-'*30)
+                COMP_SCORE = COMP_SCORE + 1
             else:
                 print("Scissor Beat paper")
                 print('-'*30)
                 print("User Won")
                 print('-'*30)
+                USER_SCORE = USER_SCORE + 1
         else:
             if bot == "Rock":
                 print("Rock Beat Scissor")
                 print('-'*30)
                 print("User Won")
                 print('-'*30)
+                USER_SCORE = USER_SCORE + 1
             else:
                 print("Scissor Beat paper")
                 print('-'*30)
                 print("User Lost")
                 print('-'*30)
+                COMP_SCORE = COMP_SCORE + 1
     time.sleep(1)
